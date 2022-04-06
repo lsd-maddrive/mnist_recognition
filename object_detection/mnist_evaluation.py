9142129a1367cc7811f965af9426605421ad0b15
MarinaKupetskova <marina.kupetskova@mail.ru>
2022-04-06 11:00:55 +0300

    [issue#42_MnistEvaluator] [NF]: mnist evaluator

--- object_detection/mnist_evaluation.py
+++ object_detection/mnist_evaluation.py
@@ -1,34 +1,35 @@
 import numpy as np
 import pandas as pd
-from sklearn.metrics import recall_score
+from sklearn.metrics import classification_report
 from tqdm import tqdm
 
 
 class MnistEvaluator:
-    def __init__(self, inference: object, data_df: list):
+    def __init__(self, inference: object, data: list):
         self._infer = inference
-        self._data = data_df
+        self._data = data
+        self._predictions = None
 
     def evaluate(self):
-        df_predicion = []
+        pred = []
         labels = []
         for img, label in tqdm(self._data):
             img = np.array(img)
-            df_predicion.append(self._infer.get_prediction(img))
+            pred.append(self._infer.get_prediction(img))
             labels.append(label)
 
         df_predicion = pd.DataFrame(labels, columns=["true_label"])
-        df_predicion["pred_label"] = df_predicion
+        df_predicion["pred_label"] = pred
+        self._predictions = df_predicion
         return df_predicion
 
-    def classification_report(self, df_prediction):
-
-        classification_report = []
-        # matrix = create_CM(df_prediction["True labels"],df_prediction["Predictions"])
-        classification_report.append(
-            recall_score(
-                df_prediction["true_label"], df_prediction["pred_label"]
+    def classification_report(self):
+        if self._predictions is None:
+            raise ValueError(
+                "Predictions were not made"
+                "\n Method .evaluate() must be called first"
             )
+        classification_r = classification_report(
+            self._predictions["true_label"], self._predictions["pred_label"]
         )
-        # classification_report = compute_classification_metrics(matrix)
-        return classification_report
+        return classification_r
