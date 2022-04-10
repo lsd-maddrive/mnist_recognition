fc7c5b270fb6ef338ab46d5353cf7f575c39f68e
MarinaKupetskova <marina.kupetskova@mail.ru>
2022-04-10 18:58:23 +0300

    [issue#42_MnistEvaluator] [BF]:Fixes in evaluator files

--- object_detection/mnist_evaluation.py
+++ object_detection/mnist_evaluation.py
@@ -1,14 +1,21 @@
+import logging
+
 import numpy as np
 import pandas as pd
 from sklearn.metrics import classification_report
 from tqdm import tqdm
 
+from object_detection import mnist_inference
+
 
 class MnistEvaluator:
-    def __init__(self, inference: object, data: list):
+    def __init__(self, inference: mnist_inference.Inference, data: list):
+        self._logger = logging.getLogger(self.__class__.__name__)
         self._infer = inference
         self._data = data
-        self._predictions = None
+        self._predictions = pd.DataFrame(
+            [], columns=["true_label", "pred_label"]
+        )
 
     def evaluate(self):
         pred = []
@@ -18,18 +25,25 @@ class MnistEvaluator:
             pred.append(self._infer.get_prediction(img))
             labels.append(label)
 
-        df_predicion = pd.DataFrame(labels, columns=["true_label"])
-        df_predicion["pred_label"] = pred
-        self._predictions = df_predicion
-        return df_predicion
+        self._predictions[self._predictions.columns] = np.column_stack(
+            (labels, pred)
+        )
+        return self._predictions
 
     def classification_report(self):
-        if self._predictions is None:
+        if self._predictions.empty:
             raise ValueError(
                 "Predictions were not made"
                 "\n Method .evaluate() must be called first"
             )
         classification_r = classification_report(
-            self._predictions["true_label"], self._predictions["pred_label"]
+            self._predictions["true_label"],
+            self._predictions["pred_label"],
+            output_dict=True,
         )
+
+        classification_r = pd.DataFrame(classification_r).round(2).T
+        classification_r.loc[
+            ["accuracy"], ["precision", "recall", "support"]
+        ] = " "
         return classification_r
